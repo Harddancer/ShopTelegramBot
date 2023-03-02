b8358fcff3deddc2a2ab10fec0e4e4d816e097c6
yamamotod <yamamotod@mail.ru>
2023-03-02 19:53:16 +0300

    add_FE

---
+++ appMainShopTelegramBot/settings/config.py
@@ -0,0 +1,55 @@
+import os
+# импортируем модуль emoji для отображения эмоджи
+from emoji import emojize
+
+# токен выдается при регистрации приложения
+TOKEN = ''
+# название БД
+NAME_DB = 'botshop.sqlite'
+# версия приложения
+VERSION = '1.0'
+# автор приложния
+AUTHOR = 'mvandron'
+
+# родительская директория до директории settings
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# путь до базы данных 
+
+DATABASE = os.path.join('sqlite:///'+BASE_DIR, "shopdb","shop.sql",NAME_DB)
+
+COUNT = 0
+
+# кнопки управления
+KEYBOARD = {
+    'CHOOSE_GOODS': emojize(':open_file_folder: Выбрать товар'),
+    'INFO': emojize(':speech_balloon: О магазине'),
+    'SETTINGS': emojize('⚙️ Настройки'),
+    'SEMIPRODUCT': emojize(':pizza: Полуфабрикаты'),
+    'GROCERY': emojize(':bread: Бакалея'),
+    'ICE_CREAM': emojize(':shaved_ice: Мороженое'),
+    '<<': emojize('⏪'),
+    '>>': emojize('⏩'),
+    'BACK_STEP': emojize('◀️'),
+    'NEXT_STEP': emojize('▶️'),
+    'ORDER': emojize('✅ ЗАКАЗ'),
+    'X': emojize('❌'),
+    'DOUWN': emojize('🔽'),
+    'AMOUNT_PRODUCT': COUNT,
+    'AMOUNT_ORDERS': COUNT,
+    'UP': emojize('🔼'),
+    'APPLAY': '✅ Оформить заказ',
+    'COPY': '©️'
+}
+
+# id категорий продуктов
+CATEGORY = {
+    'SEMIPRODUCT': 1,
+    'GROCERY': 2,
+    'ICE_CREAM': 3,
+}
+
+# названия команд
+COMMANDS = {
+    'START': "start",
+    'HELP': "help",
+}
